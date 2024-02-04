87297d6dd96123c15e6092badf9388ca4cd1e76f
Anushka Rai <anushka010mca23@igdtuw.ac.in>
2024-02-04 15:16:36 +0530

    Add sentiment analysis folder with large pickle file

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+import time
+st.title("Twitter Sentiment Analysis")
+model = pickle.load(open('twitter_sentiment (1).pkl', 'rb'))
+tweet = st.text_input("Enter your tweet")
+submit = st.button('Predict')
+
+if submit:
+    start = time.time()
+    prediction = model.predict([tweet])
+    end = time.time()
+    st.write('Prediction time taken', round(end - start, 2), 'seconds')
+    print(prediction[0])
+    st.write("Predicted sentiment is ", prediction[0])
+
